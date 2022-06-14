cfd163461c8cf525b08eb9acf15b58a93ecd3965
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2022-06-14 10:52:55 -0300

    Update TransportePublico.java

--- CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransportePublico.java
+++ CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransportePublico.java
@@ -1,4 +1,18 @@
 package Dominio.Transportes;
 
+import Dominio.Transportes.TipoDeTransportePublico;
+import Dominio.Lugares.Parada;
+
 public class TransportePublico extends Transporte {
+    private TipoDeTransportePublico tipoTransportePublico;
+    private Parada[] paradas;
+    private String linea;
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea() {
+        this.linea = linea;
+    }
 }
