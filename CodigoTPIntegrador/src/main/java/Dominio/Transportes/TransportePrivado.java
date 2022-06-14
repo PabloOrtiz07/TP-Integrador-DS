dc9ca2a980df0774ce392fbef8ae6c21e90faaa3
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2022-06-14 10:52:34 -0300

    Update TransportePrivado.java

--- CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransportePrivado.java
+++ CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransportePrivado.java
@@ -1,4 +1,29 @@
 package Dominio.Transportes;
 
+import Dominio.Transportes.TipoCombustible;
+import Dominio.Transportes.TipoVehicuoParticular;
+
 public class TransportePrivado extends Transporte{
+    private boolean esServicioContratado;
+    private String tipoServicio;
+    private TipoCombustible tipoDeCombustible;
+    private TipoVehicuoParticular tipoTransporteVehiculo;
+
+    public boolean getEsServicioContratado() {
+        return esServicioContratado;
+    }
+
+    public void setEsServicioContratado(boolean esServicioContratado) {
+        this.esServicioContratado = esServicioContratado;
+    }
+
+    public String getTipoServicio() {
+        return tipoServicio;
+    }
+
+    public void setTipoServicio() {
+        this.tipoServicio = tipoServicio;
+    }
+
 }
+
