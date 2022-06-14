e033f4bc1d330a57504ea14a93c6afa13a074762
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2022-06-14 10:52:06 -0300

    Update TransporteEcologico.java

--- CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransporteEcologico.java
+++ CodigoTPIntegrador/src/main/java/Dominio/Transportes/TransporteEcologico.java
@@ -1,4 +1,13 @@
 package Dominio.Transportes;
 
 public class TransporteEcologico extends Transporte {
+    private String tipoTransporteEcologico;
+
+    public String getTipoTransporteEcologico() {
+        return tipoTransporteEcologico;
+    }
+
+    public void setTipoTransporteEcologico(String tipoTransporteEcologico) {
+        this.tipoTransporteEcologico = tipoTransporteEcologico;
+    }
 }
