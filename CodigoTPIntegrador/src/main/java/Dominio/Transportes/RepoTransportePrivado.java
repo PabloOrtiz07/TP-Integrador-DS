84a7c5c4e86d8ece4db847de98aef536387aee93
lewislee0920 <62365146+lewislee0920@users.noreply.github.com>
2022-06-14 10:53:59 -0300

    Create RepoTransportePrivado.java

---
+++ CodigoTPIntegrador/src/main/java/Dominio/Transportes/RepoTransportePrivado.java
@@ -0,0 +1,9 @@
+package Dominio.Transportes;
+
+import Dominio.Transportes.TransportePrivado;
+
+public class RepoTransportePrivado extends TransportePrivado {
+    private TransportePrivado instance;
+    private TransportePrivado[] RepoTransportePrivado;
+
+}
