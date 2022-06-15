d5f06f6d0627d302139d203fdb0829b7bdc05e69
Pablo Ortiz <pablomiguelortiz07@gmail.com>
2022-06-15 17:10:06 -0300

    se sube lo de repo con singleton

--- CodigoTPIntegrador/src/main/java/Dominio/Entidades/RepoOrganizaciones.java
+++ CodigoTPIntegrador/src/main/java/Dominio/Entidades/RepoOrganizaciones.java
@@ -1,2 +1,21 @@
 package Dominio.Entidades;
 
+public class Reporganizaciones {
+    private Organizacion [] organizacions;
+
+    //como este metodo es privado y no tengo acceso, se puede instanciar por unica vez
+    private Reporganizaciones(){
+
+    }
+
+    public Organizacion[] getOrganizacions() {
+        return organizacions;
+    }
+
+    public void setOrganizacions(Organizacion[] organizacions) {
+        this.organizacions = organizacions;
+    }
+
+
+
+}
