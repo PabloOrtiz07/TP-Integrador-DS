42ebb270f389ad1640667bca6864494281072aa5
lopez-juli <julianlopez201313@mail.com>
2022-09-01 16:07:09 -0300

    arreglos excel

--- CodigoTPIntegrador/src/main/java/LectoresArchivo/DatoLeidoExcel.java
+++ CodigoTPIntegrador/src/main/java/LectoresArchivo/DatoLeidoExcel.java
@@ -7,11 +7,13 @@ public class DatoLeidoExcel {
     private String tipoDeConsumo;
     private Double consumo;
     private LocalDate periodoDeImputacion;
+    private String perioicidad;
 
-    public DatoLeidoExcel(String actividad, String tipoDeConsumo, Double consumo, LocalDate periodoDeImputacion) {
+    public DatoLeidoExcel(String actividad, String tipoDeConsumo, Double consumo,Sting perioicidad, LocalDate periodoDeImputacion) {
         this.actividad = actividad;
         this.tipoDeConsumo = tipoDeConsumo;
         this.consumo = consumo;
+        this.perioicidad = perioicidad;
         this.periodoDeImputacion = periodoDeImputacion;
     }
 
